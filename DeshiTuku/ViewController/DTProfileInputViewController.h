bb410212af7b58bc92ca95e059406f4ce98504ac
giginet <giginet.net@gmail.com>
2013-06-15 17:30:21 +0900

    プロフィール設定画面が乗った

---
+++ DeshiTuku/ViewController/DTProfileInputViewController.h
@@ -0,0 +1,16 @@
+//
+//  DTProfileInputViewController.h
+//  DeshiTuku
+//
+//  Created by giginet on 2013/6/15.
+//  Copyright (c) 2013 giginet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DTProfileInputViewController : UIViewController <UITextViewDelegate>
+
+@property(readwrite, nonatomic) IBOutlet UITextView *textView;
+@property(readwrite, nonatomic) IBOutlet UIButton *button;
+
+@end
---
+++ DeshiTuku/ViewController/DTProfileInputViewController.m
@@ -0,0 +1,49 @@
+//
+//  DTProfileInputViewController.m
+//  DeshiTuku
+//
+//  Created by giginet on 2013/6/15.
+//  Copyright (c) 2013 giginet. All rights reserved.
+//
+
+#import "DTProfileInputViewController.h"
+#import "DTUserManager.h"
+
+@interface DTProfileInputViewController ()
+
+@end
+
+@implementation DTProfileInputViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    self.textView.delegate = self;
+    [self.textView becomeFirstResponder];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark UITextViewDelegate
+
+- (void)textViewDidChange:(UITextView *)textView {
+    NSString *text = textView.text;
+    DTUserManager *manager = [DTUserManager sharedManager];
+    DTUser *user = [manager currentUser];
+    user.profile = text;
+}
+
+@end
