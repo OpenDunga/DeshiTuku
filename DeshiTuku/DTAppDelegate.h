a41ef054f52922a3cb2058484da47638a42276b2
giginet <giginet.net@gmail.com>
2013-06-15 12:16:08 +0900

    Initial Commit

---
+++ DeshiTuku/DTAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  DTAppDelegate.h
+//  DeshiTuku
+//
+//  Created by giginet on 2013/6/15.
+//  Copyright (c) 2013 giginet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface DTAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ DeshiTuku/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  DeshiTuku
+//
+//  Created by giginet on 2013/6/15.
+//  Copyright (c) 2013 giginet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "DTAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([DTAppDelegate class]));
+    }
+}
